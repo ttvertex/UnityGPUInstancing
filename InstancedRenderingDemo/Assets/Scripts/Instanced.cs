edccc2a26ae9828a350e6e896d1c605ed09da58e
tiiago11 <tiiago11@gmail.com>
2016-10-16 22:05:26 -0200

    added basic skeleton

---
+++ InstancedRenderingDemo/Assets/Scripts/Instanced.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Instanced : MonoBehaviour {
+	public GameObject m_Prefab;
+	public int count = 100000;
+
+	// Use this for initialization
+	void Start () {
+		MaterialPropertyBlock props = new MaterialPropertyBlock();
+		MeshRenderer renderer;
+
+		List<GameObject> objects = new List<GameObject>();
+		for(int i = 0; i < count; i++)
+		{
+			objects.Add(Instantiate(m_Prefab));
+		}
+
+		foreach (GameObject obj in objects)
+		{
+			float r = Random.Range(0.0f, 1.0f);
+			float g = Random.Range(0.0f, 1.0f);
+			float b = Random.Range(0.0f, 1.0f);
+			props.SetColor("_Color", new Color(r, g, b));
+
+			obj.transform.position = new Vector3(Random.Range(0, 1000), Random.Range(0, 1000), Random.Range(0, 1000));
+
+			renderer = obj.GetComponent<MeshRenderer>();
+			renderer.SetPropertyBlock(props);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ InstancedRenderingDemo/Assets/Scripts/Regular.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Regular : MonoBehaviour {
+
+	public GameObject m_Prefab;
+	public int count = 100000;
+
+	// Use this for initialization
+	void Start()
+	{
+		for (int i = 0; i < count; i++)
+		{
+			Instantiate(m_Prefab).transform.position = new Vector3(Random.Range(0, 1000), Random.Range(0, 1000), Random.Range(0, 1000));
+		}
+	}
+}
